b6f29701f0bba2a6437c0e237c67b5eef12e5234
Utkarsh-Jain2199 <77992826+Utkarsh-Jain2199@users.noreply.github.com>
2024-06-14 01:53:46 +0530

    Update CreateTaskModal.jsx

--- src/components/CreateTaskModal.jsx
+++ src/components/CreateTaskModal.jsx
@@ -14,7 +14,7 @@ const CreateTaskModal = ({
   const handleCreateTask = async () => {
     await axios
       .post(
-        "http://localhost:4000/api/v1/task/post",
+        "https://task-management-web-app-backend.onrender.com/api/v1/task/post",
         { title, description },
         {
           withCredentials: true,
