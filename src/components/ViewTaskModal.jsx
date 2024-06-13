7432bcfd9938090b2d2d203c67c1514b1281dbc3
Utkarsh-Jain2199 <77992826+Utkarsh-Jain2199@users.noreply.github.com>
2024-06-14 01:56:33 +0530

    Update ViewTaskModal.jsx

--- src/components/ViewTaskModal.jsx
+++ src/components/ViewTaskModal.jsx
@@ -8,7 +8,7 @@ const ViewTaskModal = ({ showViewModal, handleViewModalClose, id }) => {
   useEffect(() => {
     const getSingleTask = async () => {
       await axios
-        .get(`http://localhost:4000/api/v1/task/single/${id}`, {
+        .get(`https://task-management-web-app-backend.onrender.com/api/v1/task/single/${id}`, {
           withCredentials: true,
         })
         .then((res) => {
